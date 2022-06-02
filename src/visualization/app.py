7d99d34e4f66d9055370fbf25093824453e7d56f
Jakub Kozieł <jakub.koziel@interia.pl>
2022-06-03 00:26:20 +0200

    fix paths 4

--- src/visualization/app.py
+++ src/visualization/app.py
@@ -17,7 +17,7 @@ st.set_page_config(layout="wide")
 
 @st.cache(allow_output_mutation=True)
 def load_data():
-    names_counts = ['./counts/feb21_counts.csv',
+    names_counts = ['Russia-Ukraine-Twitter-Analysis/src/visualization/counts/feb21_counts.csv',
                     './counts/feb22_counts.csv',
                     './counts/feb23_counts.csv',
                     './counts/feb24_counts.csv',
